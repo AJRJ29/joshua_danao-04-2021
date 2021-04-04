7d614a2d81fd5aae48e72cb96dbeb4c81681d56a
Joshua Danao <josh@Joshuas-MacBook-Air.local>
2021-04-04 12:42:57 -0700

    started next js

---
+++ joshua_danao-04-2021/mwi-mockup/src/containers/homepage.js
@@ -0,0 +1,49 @@
+
+export function Homepage() {
+  return(
+    <div>
+      <div>
+        <img src="./mwi-logo.png" alt="" />
+      </div>
+      <div>
+        <h1>Heading One</h1>
+        <div>
+          <div></div>
+          <h2>Heading 2</h2>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime magni natus dignissimos iusto autem quibusdam minima ullam eaque voluptatibus molestias! Minus eius hic accusantium, fuga animi alias laudantium perspiciatis soluta.</p>
+          <button>Read More</button>
+        </div>
+        <div>
+          <div></div>
+          <h2>Heading 2</h2>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime magni natus dignissimos iusto autem quibusdam minima ullam eaque voluptatibus molestias! Minus eius hic accusantium, fuga animi alias laudantium perspiciatis soluta.</p>
+          <button>Read More</button>
+        </div>
+        <div>
+          <div></div>
+          <h2>Heading 2</h2>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime magni natus dignissimos iusto autem quibusdam minima ullam eaque voluptatibus molestias! Minus eius hic accusantium, fuga animi alias laudantium perspiciatis soluta.</p>
+          <button>Read More</button>
+        </div>
+      </div>
+      <div>
+        <h1>Contact</h1>
+        <form action="">
+          <h4>
+            Email
+            <input type="text" />
+          </h4>
+          <h4>
+            Subject
+            <input type="text" />
+          </h4>
+          <h4>
+            Message
+            <input type="text" />
+          </h4>
+          <button>Submit</button>
+        </form>
+      </div>
+    </div>
+  )
+}
