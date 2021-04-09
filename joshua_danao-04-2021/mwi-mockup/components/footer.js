5cb977b3dc8021118849174b20002d9986c4f8b8
Joshua Danao <josh@Joshuas-MacBook-Air.local>
2021-04-09 13:39:44 -0700

    change link color

--- joshua_danao-04-2021/mwi-mockup/components/footer.js
+++ joshua_danao-04-2021/mwi-mockup/components/footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
   return(
     <div className={styles.footer}>
       <h1 className={styles.banner}>HEADING ONE</h1>
-      <p className={styles.content}>Remove the duplicate in 2 Javascript objects and output the list of distinct names in an unordered list when this <a href=""> link is clicked</a>, if the operation has been completed already notify the user that this has already been done.</p>
+      <p className={styles.content}>Remove the duplicate in 2 Javascript objects and output the list of distinct names in an unordered list when this <a className={styles.contentLink} href=""> link is clicked</a>, if the operation has been completed already notify the user that this has already been done.</p>
       <div className={styles.footbar}>
         <h4>Midwestern Interactive Benchmark Test</h4>
       </div>
