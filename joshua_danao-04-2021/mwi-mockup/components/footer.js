bb2c7521940bfe4b3951270b447ff43e72f86bdc
Joshua Danao <josh@Joshuas-MacBook-Air.local>
2021-04-11 02:00:33 -0700

    solve puzzle

--- joshua_danao-04-2021/mwi-mockup/components/footer.js
+++ joshua_danao-04-2021/mwi-mockup/components/footer.js
@@ -1,13 +1,58 @@
-import styles from '../styles/footer.module.css'
+import React, { useState, useEffect } from 'react';
+import styles from '../styles/footer.module.css';
+
+let object1 = [
+  {id: 0, firstName: "Matt", lastName: "Johnson"}, 
+  {id: 1, firstName: "Bart", lastName: "Paden"}, 
+  {id: 2, firstName: "Ryan", lastName: "Doss"}, 
+  {id: 3, firstName: "Jared", lastName: "Malcolm"}
+],
+ object2 = [
+  {id: 0, firstName: "Matt", lastName: "Johnson"}, 
+  {id: 1, firstName: "Bart", lastName: "Paden"}, 
+  {id: 6, firstName: "Jordan", lastName: "Heigle"}, 
+  {id: 7, firstName: "Tyler", lastName: "Viles"}
+];
+
+let newArray = [...object1, ...object2];
+
+let uniqNames = [...newArray.reduce((map, obj) => map.set(obj.id, obj),new Map()).values()];
 
 export default function Footer() {
+  const [filterName, setFilterNames] = useState()
+
+const handleClick = (e) => {
+  e.preventDefault()
+  setFilterNames(uniqNames)
+} 
+
+if(filterName) {
+  return(
+    <div className={styles.footer}>
+    <h1 className={styles.banner}>HEADING ONE</h1>
+    <p className={styles.content}> <a className={styles.contentLink} onClick={handleClick}href=""> click here</a></p>
+    <ul className={styles.contentNames}>
+      {uniqNames.map((uniqNames, i) => (
+        <li key={i}>
+          <h1>{uniqNames.firstName} {uniqNames.lastName}</h1>
+        </li>
+      ))}
+     </ul>
+    <div className={styles.footbar}>
+      <h4>Midwestern Interactive Benchmark Test</h4>
+    </div>
+  </div>
+  )
+} else {
   return(
     <div className={styles.footer}>
       <h1 className={styles.banner}>HEADING ONE</h1>
-      <p className={styles.content}>Remove the duplicate in 2 Javascript objects and output the list of distinct names in an unordered list when this <a className={styles.contentLink} href=""> link is clicked</a>, if the operation has been completed already notify the user that this has already been done.</p>
+      <p className={styles.content}><a className={styles.contentLink} onClick={handleClick}href=""> click here</a></p>
       <div className={styles.footbar}>
         <h4>Midwestern Interactive Benchmark Test</h4>
       </div>
     </div>
   )
+  }
 }
+
